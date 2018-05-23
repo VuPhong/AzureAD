d13525bdad27835a0160a22920ac667e99b7e570
Vu Phong <vuphong1092@gmail.com>
2018-05-23 14:19:27 +0900

    Provide quick way to enable bundling and minification.

--- AzureADTest/App_Start/BundleConfig.cs
+++ AzureADTest/App_Start/BundleConfig.cs
@@ -26,6 +26,9 @@ namespace AzureADTest
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/Site.css"));
+
+            //To enable bundling and minification, uncomment below line.
+            //BundleTable.EnableOptimizations = true;
         }
     }
 }
